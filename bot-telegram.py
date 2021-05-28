600153443a4c1a1274b799ff5ef86e6c8a747377
bagdeabhishek <bagdeabhishek@gmail.com>
2021-05-29 02:06:37 +0530

    Final touches

--- bot-telegram.py
+++ bot-telegram.py
@@ -4,7 +4,7 @@ import logging
 from telegram.ext import CommandHandler
 import requests
 NGROK_URL = "http://127.0.0.1:4040/api/tunnels"
-PUBLIC_URL_SUCCCESS_MESSAGE = "The public urls are {0}"
+PUBLIC_URL_SUCCCESS_MESSAGE = "The public urls are \n ---------------------- \n{0}"
 PUBLIC_URL_FAILURE_MESSAGE = "The server isn't running currently! Ping @bagdeabhishek"
 def get_public_urls():
     response = requests.get(NGROK_URL)
@@ -12,8 +12,9 @@ def get_public_urls():
         if response.status_code != 200:
             return None
         public_urls = []
+        response = response.json()
         for tunnel in response.get('tunnels'):
-            public_urls.append(tunnel.get('public_url'))
+            public_urls.append(tunnel.get('name')+ " : " + tunnel.get('public_url'))
         return public_urls
     except Exception:
         logging.error("Exception occured", exc_info=True)
@@ -21,7 +22,7 @@ def get_public_urls():
 def url(update, context):
     urls = get_public_urls()
     if urls:
-        public_urls_string = ",".join(urls)
+        public_urls_string = "\n".join(urls)
         context.bot.send_message(chat_id=update.effective_chat.id, text=PUBLIC_URL_SUCCCESS_MESSAGE.format(public_urls_string))
     else:
         context.bot.send_message(chat_id=update.effective_chat.id, text=PUBLIC_URL_FAILURE_MESSAGE)
